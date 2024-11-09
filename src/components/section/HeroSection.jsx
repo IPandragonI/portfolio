9fefbc0bf327da41a77d6eaa3b91c0793be0b7a0
Mathys Farineau <mathys.farineau@etu.univ-grenoble-alpes.fr>
2024-11-09 23:39:13 +0100

    Added a rubiks

--- src/components/section/HeroSection.jsx
+++ src/components/section/HeroSection.jsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
 
         if (window.innerWidth < 768) {
             screenScale = [1.2, 1.2, 1.2];
-            screenPosition = [0, -1.7, 0];
+            screenPosition = [0, -2.1, 0];
         } else {
             screenScale = [1.5, 1.5, 1.5];
             screenPosition = [2, -1.7, 0];
@@ -28,16 +28,17 @@ const HeroSection = () => {
 
     return (
         <>
-            <div className='flex flex-col w-10/12 h-5/6 absolute z-10'>
-                <nav className='flex justify-end items-center'>
+            <div className='flex flex-col w-full md:px-0 h-full py-10 md:h-5/6 md:py-0 absolute z-10'>
+                <nav className='flex justify-between items-center mx-0 md:mx-40'>
+                    <img src="images/rubik.png" alt="cube" className="w-12 h-12 mx-4"/>
                     <LanguageSwitcher/>
                 </nav>
-                <div className="flex flex-col relative top-10 md:top-40 items-center md:items-start">
+                <div className="flex flex-col relative w-full mx-10 xl:mx-40 top-20 md:top-40 items-start">
                     <h1 className="-mb-4 md:-mb-6">{t('heroSection_title_1')}</h1>
                     <h1>{t('heroSection_title_2')}</h1>
                     <div className="my-4">
-                        <h2 className="text-center md:text-left">{t('heroSection_legend_1')}</h2>
-                        <h2 className="text-center md:text-left">{t('heroSection_legend_2')}</h2>
+                        <h2>{t('heroSection_legend_1')}</h2>
+                        <h2>{t('heroSection_legend_2')}</h2>
                     </div>
                 </div>
             </div>
