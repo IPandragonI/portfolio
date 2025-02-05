762be354cc6a07fdcb0865a314c71378f359896f
IPandragonI <mathysf84pro@gmail.com>
2025-02-05 10:44:29 +0100

    Edit i18n

--- src/i18n.js
+++ src/i18n.js
@@ -19,7 +19,7 @@ i18n
     .use(initReactI18next)
     .init({
         resources,
-        fallbackLng: 'fr',
+        fallbackLng: 'en',
         // interpolation: {
         //     escapeValue: false
         // }
