b72643f18e26f91332e74bc6fe12cb12009df80c
IPandragonI <mathysf84pro@gmail.com>
2026-01-08 08:02:22 +0100

    Refactor ContactSection to remove clipboard functionality and update email link

--- src/components/section/ContactSection.jsx
+++ src/components/section/ContactSection.jsx
@@ -1,18 +1,8 @@
-import { useState } from 'react';
-
 const ContactSection = () => {
-    const [isPopupVisible, setIsPopupVisible] = useState(false);
     const email = 'mathysf84pro@gmail.com';
     const linkedinLink = 'https://www.linkedin.com/in/mathys-farineau/';
     const cvLink = 'docs/Mathys_FARINEAU.pdf';
     const githubLink = 'https://github.com/IPandragonI';
-
-    const copyToClipboard = () => {
-        navigator.clipboard.writeText(email).then(() => {
-            setIsPopupVisible(true);
-            setTimeout(() => setIsPopupVisible(false), 1000);
-        });
-    };
 
     return (
         <div className="flex flex-col w-9/12 py-10 h-3/6 md:py-0 absolute z-10">
@@ -20,8 +10,7 @@ const ContactSection = () => {
             <hr className="h-[2px] my-4 rounded-3xl bg-primary border-0"/>
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
                 <ContactItem title="LIKEDIN" content="mathys-farineau" link={linkedinLink} />
-                <ContactItem title="EMAIL" content={email} onClick={copyToClipboard} />
-                {isPopupVisible && <Popup message="Copié !" />}
+                <ContactItem title="EMAIL" content={email} />
                 <ContactItem title="GITHUB" content="IPandragonI" link={githubLink} />
                 <ContactItem title="CV" content="Mon CV" link={cvLink} />
             </div>
@@ -35,16 +24,8 @@ const ContactItem = ({ title, content, onClick, link }) => (
         {link ? (
             <a className="text-xs lg:text-xl xl:text-3xl text-text-color font-light relative inline-block link-underline decoration-0 after:absolute after:w-0 after:h-[.15rem] after:block after:mt-1 after:right-0 after:bg-primary after:transition[width-right] after:ease-in after:duration-300 hover:after:w-full hover:after:right-0 cursor-hover" href={link} target="_blank">{content}</a>
         ) : (
-            <a href="#" className="text-xs lg:text-xl xl:text-3xl text-text-color font-light relative inline-block decoration-0 link-underline after:absolute after:w-0 after:h-[.15rem] after:block after:mt-1 after:right-0 after:bg-primary after:transition[width-right] after:ease-in after:duration-300 hover:after:w-full hover:after:right-0 cursor-hover" onClick={onClick}>{content}</a>
+            <a target="_blank" href="mailto:{email}" className="text-xs lg:text-xl xl:text-3xl text-text-color font-light relative inline-block decoration-0 link-underline after:absolute after:w-0 after:h-[.15rem] after:block after:mt-1 after:right-0 after:bg-primary after:transition[width-right] after:ease-in after:duration-300 hover:after:w-full hover:after:right-0 cursor-hover" onClick={onClick}>{content}</a>
         )}
-    </div>
-);
-
-const Popup = ({ message }) => (
-    <div className="absolute mb-2 transform translate-x-[60rem] -translate-y-20 bg-neutral-800 p-2 rounded shadow text-xs">
-        <div className="relative">
-            <p className="text-gray-100">{message}</p>
-        </div>
     </div>
 );
 
